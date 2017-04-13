d719cfe7aa8849af4c93db45bf3153ad9e706309
Jake Demian <jakedemian@gmail.com>
2017-04-13 02:13:23 -0400

    NOT COMPLETE forgot to push this new dependency with the last commit

---
+++ Assets/Scripts/Classes/ScreenHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenHelper : MonoBehaviour {
+
+	public static Vector2 getScreenDimensions() {
+		return new Vector2(Screen.width, Screen.height);
+	}
+
+	public static Vector2 getScreenMidpoint() {
+		Vector2 screenDimensions = getScreenDimensions();
+		return new Vector2(screenDimensions.x / 2, screenDimensions.y / 2);
+	}
+}
